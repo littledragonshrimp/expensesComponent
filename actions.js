4b5d977c31e36f6e94aba502478205f7024f217c
Jordan <JDLChang@gmail.com>
2023-05-11 09:55:57 -0500

    mutes js

--- actions.js
+++ actions.js
@@ -10,26 +10,26 @@ const data = fetch('data.json')
 // Convert data into array
 let chartContainer = document.querySelector('.chart-container');
 
-// Map through data and populate chart
-data.then(data => {
-    data.forEach(item => {
-        let chart = document.createElement('div');
-        chart.classList.add('chart');
-        const height = item.amount / 7;
-        let value=""
-        if (item.day == "wed") {
-            value=`<div class="chart-value active" style="--height:${height}em"></div>`
-        } else {
-            value=`<div class="chart-value" style="--height:${height}em"></div>`;
-        }
-        chart.innerHTML = `
-        <div class="chart-wrapper">
-        ${value}
-        </div>
-        <div class="chart-title">${item.day}</div>
-        `;
-        chartContainer.appendChild(chart);
-    }
-    )
-}
-)
+// // Map through data and populate chart
+// data.then(data => {
+//     data.forEach(item => {
+//         let chart = document.createElement('div');
+//         chart.classList.add('chart');
+//         const height = item.amount / 7;
+//         let value=""
+//         if (item.day == "wed") {
+//             value=`<div class="chart-value active" style="--height:${height}em"></div>`
+//         } else {
+//             value=`<div class="chart-value" style="--height:${height}em"></div>`;
+//         }
+//         chart.innerHTML = `
+//         <div class="chart-wrapper">
+//         ${value}
+//         </div>
+//         <div class="chart-title">${item.day}</div>
+//         `;
+//         chartContainer.appendChild(chart);
+//     }
+//     )
+// }
+// )
